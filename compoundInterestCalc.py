000b865457b2aff47ce1111b0032de09cc423218
Shrish-MCVTS <shrish.kushwaha@mcvts.org>
2025-10-29 10:43:07 -0400

    AddedPlotting

--- compoundInterestCalc.py
+++ compoundInterestCalc.py
@@ -3,6 +3,13 @@
 #Compound Interest Calculator
 #Extra:
 import turtleGraphic
+import turtle
+t = turtle.Turtle()
+t.speed(0)
+
+screen = turtle.Screen()
+screen.setup(width = 800 , height = 800)
+offset = 100
 
 principalAmount = float(input("How much money are currently investing?"))
 timesCompoundedPerYear = float(input("How many times is this investment getting compounded per year?"))
@@ -16,4 +23,8 @@ interest = futureValue-principalAmount
 print(f'\033[91mThe future value of your investment is:{futureValue :,.2f}$\nThe interest/profit gained is: {interest: ,.2f}$\033[0m')
 
 
-turtleGraphic.drawCordPlane(yearsInvested//2,futureValue//5)
+turtleGraphic.drawCordPlane(yearsInvested//2,futureValue//5, t)
+turtleGraphic.plotPoints(yearsInvested,futureValue, yearsInvested//2,futureValue//5, t)
+
+turtle.done()
+
--- turtleGraphic.py
+++ turtleGraphic.py
@@ -1,36 +1,50 @@
 import turtle
-t = turtle.Turtle()
-t.speed(0)
-screen = turtle.Screen()
-screen.setup(width = 800 , height = 800)
-offset = 100
 
-def plotPoints(x,y):
+origin = (-300, -300)
+STEP = 50
+
+def plotPoints(x,y,nX,nY,t):
     t.penup()
-    t.goto(x,y)
+    wX, wY = convertToGraphCords(x, y,nX,nY)
+    t.goto(wX, wY)
     t.pendown()
-    t.dot(5)
-def drawCordPlane(xInterval,yInterval):
+    t.dot(10)
+
+def convertToGraphCords(graphX,  graphY,GRAPH_STEP_X,GRAPH_STEP_Y):
+    x = origin[0] + (graphX * (STEP/GRAPH_STEP_X))
+    y = origin[1] + (graphY * (STEP/GRAPH_STEP_Y))
+    return x, y  
+
+def drawCordPlane(xInterval,yInterval, t):
     t.hideturtle()
     t.penup()
     t.goto(300,-300)
     t.pendown()
-    t.write('Time')
+    t.write('Time(years)')
     t.goto(-300,-300)
     t.goto(-300,300)
-    t.write('Money')
+    t.write('Money($)')
     for i in range(12):
-        intervalCreatorY(-300,i*50-250,yInterval*(i+1))
+        intervalCreatorY(-300,i*50-250,yInterval*(i+1) , t)
     for i in range(12):
-        intervalCreatorX(i*50-250,-300,xInterval*(i+1))
-    screen.mainloop()
-def intervalCreatorY(x,y,yInterval):
+        intervalCreatorX(i*50-250,-300,xInterval*(i+1) , t)
+
+def intervalCreatorY(x,y,yInterval , t):
+    t.penup()
     t.goto(x,y)
+    t.pendown()
     t.goto(x-10,y)
-    t.write(yInterval, move=True)
+    t.penup()
+    t.goto(x-30,y)
+    t.pendown()
+    t.write(yInterval, align='center')
+    t.penup()
+    t.goto(x-10,y)
+    t.pendown()
     t.goto(x+625,y)
     t.goto(x,y)
-def intervalCreatorX(x,y,xInterval):
+
+def intervalCreatorX(x,y,xInterval , t):
     t.penup()
     t.goto(x,y)
     t.pendown()
