82bcb314524e14fe127b62c096f2a611a38621db
Shrish-MCVTS <Shrish.kushwaha@mcvts.org>
2025-10-30 17:20:13 -0400

    Comments

--- compoundInterestCalc.py
+++ compoundInterestCalc.py
@@ -2,29 +2,34 @@
 #Date: 10/20/2025
 #Compound Interest Calculator
 #Extra:Used turtle graphics to plot the growth of investment over time
+
+#Importing turtleGraphic module
 import turtleGraphic
 import turtle
 t = turtle.Turtle()
 t.speed(0)
-
+#Setting up the turtle screen
 screen = turtle.Screen()
 screen.setup(width = 800 , height = 800)
+#Variables
 offset = 100
 futureValue = []
-
+#User Inputs
 principalAmount = float(input("How much money are currently investing?"))
 principalAmountCOPY = principalAmount
 timesCompoundedPerYear = float(input("How many times is this investment getting compounded per year?"))
 yearsInvested = int(input("How many years will this investment be invested?"))
 interestRate = float(input("What is the annual interest rate?(Type decimal form)"))
 interestMade = 0
+#Calculations
 for i in range(yearsInvested):
     principalAmount = principalAmount * (1 + (interestRate / timesCompoundedPerYear))**(timesCompoundedPerYear*yearsInvested)
     futureValue.append(principalAmount)
 interestMade = futureValue[0] - principalAmountCOPY
+#Output
 print(f'\033[91mThe future value of your investment is:{futureValue[0] :,.2f}$\nThe interest/profit gained is:{interestMade: ,.2f}$\033[0m')
 
-
+#Plotting the graph using turtle graphics
 turtleGraphic.drawCordPlane(yearsInvested//2,futureValue[0]//5, t)
 for i in range(yearsInvested):
     turtleGraphic.plotPoints(yearsInvested*(i+1),futureValue[i], yearsInvested//2,futureValue[0]//5, t)
--- turtleGraphic.py
+++ turtleGraphic.py
@@ -1,20 +1,20 @@
 import turtle
-
+# Turtle graphics module to plot points and draw coordinate plane
 origin = (-300, -300)
 STEP = 50
-
+# Function to plot points on the graph
 def plotPoints(x,y,nX,nY,t):
     t.penup()
     wX, wY = convertToGraphCords(x, y,nX,nY)
     t.goto(wX, wY)
     t.pendown()
     t.dot(10)
-
+# Function to convert graph coordinates to turtle screen coordinates
 def convertToGraphCords(graphX,  graphY,GRAPH_STEP_X,GRAPH_STEP_Y):
     x = origin[0] + (graphX * (STEP/GRAPH_STEP_X))
     y = origin[1] + (graphY * (STEP/GRAPH_STEP_Y))
     return x, y  
-
+# Function to draw the coordinate plane
 def drawCordPlane(xInterval,yInterval, t):
     t.hideturtle()
     t.penup()
@@ -28,7 +28,7 @@ def drawCordPlane(xInterval,yInterval, t):
         intervalCreatorY(-300,i*50-250,yInterval*(i+1) , t)
     for i in range(12):
         intervalCreatorX(i*50-250,-300,xInterval*(i+1) , t)
-
+# Function to create intervals on the axes
 def intervalCreatorY(x,y,yInterval , t):
     t.penup()
     t.goto(x,y)
